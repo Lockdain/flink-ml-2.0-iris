ed10ee54b063992ab1ee60b9cbd6805edbec7cd6
alexsergeenko <lockdain@gmail.com>
2022-06-01 11:20:32 +0300

    KNN works / DenseVector SQL edited

--- src/main/java/ru/asergeenko/flink/ml/iris/App.java
+++ src/main/java/ru/asergeenko/flink/ml/iris/App.java
@@ -2,6 +2,8 @@ package ru.asergeenko.flink.ml.iris;
 
 import org.apache.flink.ml.classification.logisticregression.LogisticRegression;
 import org.apache.flink.ml.classification.logisticregression.LogisticRegressionModel;
+import org.apache.flink.ml.classification.naivebayes.NaiveBayes;
+import org.apache.flink.ml.classification.naivebayes.NaiveBayesModel;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.Table;
@@ -29,7 +31,7 @@ public class App {
                 "species DOUBLE" +
                 ") WITH (" +
                 "'connector'='filesystem'," +
-                "'path'='src/main/resources/iris_dataset0.csv'," +
+                "'path'='src/main/resources/iris_dataset.csv'," +
                 "'format'='csv'," +
                 "'csv.ignore-parse-errors'='true'," +
                 "'csv.allow-comments'='true'" +
@@ -43,20 +45,24 @@ public class App {
 
         tableEnv.createTemporarySystemFunction("dense", dense);
 
-        Table features = tableEnv.sqlQuery("SELECT dense(sepal_length, sepal_width, petal_length, petal_width) as features, species as labels FROM iris_csv");
-        features.execute().print();
+        Table trainFeatures = tableEnv.sqlQuery("SELECT dense(sepal_length, sepal_width, petal_length, petal_width) as features, species as label FROM iris_csv");
+        Table predictFeatures = trainFeatures.dropColumns("label");
+        trainFeatures.execute().print();
+        predictFeatures.execute().print();
+
         //virginica = 3; versicolor = 2; setosa = 1;
-        LogisticRegression logisticRegression = new LogisticRegression()
-                .setFeaturesCol("features")
-                .setLabelCol("labels")
-                .setMaxIter(15);
+        NaiveBayes estimator =
+                new NaiveBayes()
+                        .setSmoothing(1.0)
+                        .setFeaturesCol("features")
+                        .setLabelCol("label")
+                        .setPredictionCol("prediction")
+                        .setModelType("multinomial");
 
-        LogisticRegressionModel model = logisticRegression.fit(features);
+        NaiveBayesModel model = estimator.fit(trainFeatures);
+        Table outputTable = model.transform(predictFeatures)[0];
 
+        outputTable.execute().print();
 
-        Table output = model.transform(features)[0];
-        Table features1 = output.select("features");
-        TableResult result = features1.execute();
-        result.print();
     }
 }
--- src/main/java/ru/asergeenko/flink/ml/iris/DenseVectorMapFunction.java
+++ src/main/java/ru/asergeenko/flink/ml/iris/DenseVectorMapFunction.java
@@ -1,25 +1,13 @@
 package ru.asergeenko.flink.ml.iris;
 
-import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.ml.linalg.typeinfo.DenseVectorTypeInfo;
+import org.apache.flink.ml.linalg.DenseVector;
+import org.apache.flink.ml.linalg.Vectors;
 import org.apache.flink.table.annotation.DataTypeHint;
-import org.apache.flink.table.api.DataTypes;
-import org.apache.flink.table.catalog.DataTypeFactory;
 import org.apache.flink.table.functions.ScalarFunction;
-import org.apache.flink.table.types.extraction.TypeInferenceExtractor;
-import org.apache.flink.table.types.inference.TypeInference;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class DenseVectorMapFunction extends ScalarFunction {
-    public @DataTypeHint("ARRAY<DOUBLE>")
-    ArrayList<Double> eval(Double a, Double b, Double c, Double d) {
-        ArrayList vector = new ArrayList<Double>(4);
-        vector.add(a);
-        vector.add(b);
-        vector.add(c);
-        vector.add(d);
-        return vector;
+    public @DataTypeHint(value = "RAW", bridgedTo = DenseVector.class)
+    DenseVector eval(Double a, Double b, Double c, Double d) {
+        return  Vectors.dense(a, b, c, d);
     }
 }
